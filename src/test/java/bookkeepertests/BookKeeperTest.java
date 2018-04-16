ca9b5ef69dfb1c740dcb0052c05980d0948a22b0
Mayar Karkout <204641@edu.p.lodz.pl>
2018-04-16 17:39:13 +0200

    Isolation: created behavioral BookKeeper test for issuance method

--- src/test/java/bookkeepertests/BookKeeperTest.java
+++ src/test/java/bookkeepertests/BookKeeperTest.java
@@ -9,6 +9,13 @@ import java.util.Date;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import org.junit.*;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
+import org.mockito.runners.MockitoJUnitRunner;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.*;
 import pl.com.bottega.ecommerce.sales.domain.invoicing.*;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.*;
@@ -18,10 +25,14 @@ import pl.com.bottega.ecommerce.sharedkernel.*;
  *
  * @author karko
  */
+@RunWith(MockitoJUnitRunner.class)
 public class BookKeeperTest {
     
+    @Mock
+    TaxPolicy taxPolicyMock;
+    
     InvoiceRequest invoiceRequest;
-    RequestItem requestItem;
+    RequestItem requestItemOne, requestItemTwo;
     ProductData productData;
     ClientData clientData;
     BookKeeper bookKeeper;
@@ -34,7 +45,8 @@ public class BookKeeperTest {
         Id id = Id.generate();
         clientData = new ClientData(id, "testname");
         productData = new ProductData(id, new Money(8), "tes-product-data", ProductType.STANDARD, new Date());
-        requestItem = new RequestItem(productData, 10, new Money(80));
+        requestItemOne = new RequestItem(productData, 10, new Money(80));
+        requestItemTwo = new RequestItem(productData, 10, new Money(80));
         invoiceRequest = new InvoiceRequest(clientData);
         taxPolicy = new TaxPolicy() {
             @Override
@@ -47,9 +59,28 @@ public class BookKeeperTest {
     
     @Test
     public void issuanceMethodCallWithOnePositionShouldReturnInvoiceWithOnePosition() {
-        invoiceRequest.add(requestItem);
+        invoiceRequest.add(requestItemOne);
 
         Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
         assertThat(invoice.getItems().size(), is(1));
+    }
+    
+    @Captor 
+    ArgumentCaptor <ProductType> argumentCaptorProductType;
+    @Captor 
+    ArgumentCaptor <Money> argumentCaptorMoney;
+    
+    @Test
+    public void issuanceMethodCallWithTwoPositionRequestShouldCallCalculateTaxMethodTwice() {
+        invoiceRequest.add(requestItemOne);
+        invoiceRequest.add(requestItemTwo);
+
+        when(taxPolicyMock.calculateTax(
+                Mockito.any(ProductType.class), Mockito.any(Money.class))).
+                thenReturn(new Tax(new Money(5), "test tax"));
+
+        bookKeeper.issuance(invoiceRequest, taxPolicyMock);
+        verify(taxPolicyMock, times(2)).
+                calculateTax(argumentCaptorProductType.capture(), argumentCaptorMoney.capture());
     }
 }
