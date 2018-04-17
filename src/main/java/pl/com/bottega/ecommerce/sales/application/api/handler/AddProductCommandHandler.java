2ec18b7c8deaab147ecd784c66b30f3005199ee4
Mayar Karkout <204641@edu.p.lodz.pl>
2018-04-17 11:26:38 +0200

    Created two tests for AddProductCommand.

--- src/main/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandler.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandler.java
@@ -30,41 +30,32 @@ import pl.com.bottega.ecommerce.system.application.SystemContext;
 
 public class AddProductCommandHandler implements CommandHandler<AddProductCommand, Void>{
 
+    private ReservationRepository reservationRepository;
+    private ProductRepository productRepository;
+    private SuggestionService suggestionService;
+    private ClientRepository clientRepository;
+    private SystemContext systemContext;
 
-	private ReservationRepository reservationRepository;
-	
+    @Override
+    public Void handle(AddProductCommand command) {
+        Reservation reservation = reservationRepository.load(command.getOrderId());
 
-	private ProductRepository productRepository;
-	
+        Product product = productRepository.load(command.getProductId());
 
-	private SuggestionService suggestionService;
-	
+        if (! product.isAvailable()){
+                Client client = loadClient();	
+                product = suggestionService.suggestEquivalent(product, client);
+        }
 
-	private ClientRepository clientRepository;
-	
+        reservation.add(product, command.getQuantity());
 
-	private SystemContext systemContext;
-	
-	@Override
-	public Void handle(AddProductCommand command) {
-		Reservation reservation = reservationRepository.load(command.getOrderId());
-		
-		Product product = productRepository.load(command.getProductId());
-		
-		if (! product.isAvailable()){
-			Client client = loadClient();	
-			product = suggestionService.suggestEquivalent(product, client);
-		}
-			
-		reservation.add(product, command.getQuantity());
-		
-		reservationRepository.save(reservation);
-		
-		return null;
-	}
-	
-	private Client loadClient() {
-		return clientRepository.load(systemContext.getSystemUser().getClientId());
-	}
+        reservationRepository.save(reservation);
+
+        return null;
+    }
+
+    private Client loadClient() {
+            return clientRepository.load(systemContext.getSystemUser().getClientId());
+    }
 
 }
---
+++ src/test/java/addproductcommandhandlertests/AddProductCommandHandlerTest.java
@@ -0,0 +1,101 @@
+package addproductcommandhandlertests;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.internal.util.reflection.Whitebox;
+import org.mockito.runners.MockitoJUnitRunner;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
+import pl.com.bottega.ecommerce.sales.domain.client.ClientRepository;
+import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductRepository;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation;
+import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import java.util.Date;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.mockito.Captor;
+import static org.mockito.Matchers.any;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import pl.com.bottega.ecommerce.sales.application.api.handler.AddProductCommandHandler;
+
+
+/**
+ *
+ * @author karko
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class AddProductCommandHandlerTest {
+    
+    
+    AddProductCommandHandler commandHandler;
+    AddProductCommand command;
+    int quantity;
+    Reservation reservation;
+    ClientData clientData;
+    
+    
+    @Mock
+    ReservationRepository reservationRepositoryMock;
+    @Mock
+    ProductRepository productRepositoryMock;
+    @Mock
+    SuggestionService suggestionServiceMock;
+    @Mock
+    ClientRepository clientRepositoryMock;
+    
+    @Before
+    public void setUp(){
+        commandHandler = new AddProductCommandHandler();
+        clientData = new ClientData(Id.generate(), "test-client");
+        command = new AddProductCommand(Id.generate(), Id.generate(), 10);
+        
+        reservation = new Reservation(Id.generate(), Reservation.ReservationStatus.OPENED,
+                                                clientData, new Date());
+        when(reservationRepositoryMock.load(any(Id.class))).thenReturn(reservation);        
+        
+
+        Product product = new Product(Id.generate(), new Money(10),
+                                    "test product name", ProductType.FOOD);
+        when(productRepositoryMock.load(any(Id.class))).thenReturn(product);
+
+        //addProductCommand = new AddProductCommand(orderId, productId, quantity);
+        Whitebox.setInternalState(commandHandler, "reservationRepository", reservationRepositoryMock);
+        Whitebox.setInternalState(commandHandler, "productRepository", productRepositoryMock);
+
+    }
+    
+    //State tests
+    @Captor
+    ArgumentCaptor <Id> argumentCaptorId;
+    
+    @Test
+    public void handleMethodCallShouldCallReservationRepositoryLoadOnce() {
+        commandHandler.handle(command);
+        
+        verify(reservationRepositoryMock, times(1)).load(argumentCaptorId.capture());
+        assertThat(argumentCaptorId.getValue(), is(command.getOrderId()));
+    }
+    
+    @Test
+    public void handleMethodCallShouldCallReservationSaveOnce() {
+        commandHandler.handle(command);
+        
+        verify(reservationRepositoryMock, times(1)).save(Mockito.any(Reservation.class));
+    }
+    
+}
